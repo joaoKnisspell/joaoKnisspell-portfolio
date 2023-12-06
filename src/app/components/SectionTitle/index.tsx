41603a71e7bb8fe8243e05eb96b940b826d8ba79
João Gabriel Knisspell Zimmermann <joaokz@outlook.com>
2023-12-06 16:56:22 -0300

    component SectionTitle fixed.

--- src/app/components/SectionTitle/index.tsx
+++ src/app/components/SectionTitle/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function SectionTitle(props: { title: string }) {
   return (
-    <h3 className="mt-4 text-center text-2xl font-semibold text-black dark:text-zinc-100 md:mb-4 md:mt-0 md:text-3xl">
+    <h3 className="mt-4 text-center text-2xl font-semibold text-black md:mb-4 md:mt-0 md:text-3xl">
       {props.title}
     </h3>
   )
